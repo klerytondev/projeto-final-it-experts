c204f9bdf18375448011a2f4ac779f674721fa3c
Laura Girdwood <azevedoemello@gmail.com>
2022-02-25 13:34:28 -0300

    adicionando métodos de teste para estudo + adicionando método delte

--- projeto/projeto/src/main/java/com/itexperts/projeto/controller/AccountController.java
+++ projeto/projeto/src/main/java/com/itexperts/projeto/controller/AccountController.java
@@ -4,10 +4,10 @@ import com.itexperts.projeto.model.Account;
 import com.itexperts.projeto.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
@@ -16,26 +16,53 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
-    @GetMapping
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<Account> listAccounts() {
-        return accountRepository.findAll();
-    }
-
-//    @GetMapping
-//    public String listAccounts() {
-//        return "Hello World";
-//    }
-
-    @GetMapping("/{id}")
-    public String getAccountById(@PathVariable Integer id) {
-        return "Hello World by id: " + id;
-    }
-
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public Account createAccount(@RequestBody Account account) {
+        System.out.println("Criando uma conta");
         return accountRepository.save(account);
     }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Optional<Account> getOneAccountById(@PathVariable Integer id) {
+        System.out.println("Pegando uma única conta por id");
+        return accountRepository.findById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Account> listAllAccounts() {
+        System.out.println("Listando todas as contas");
+        return accountRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.)
+    public void deleteOneAccountById(@PathVariable Integer id) {
+        System.out.println("Deletando uma única conta por id");
+        accountRepository.deleteById(id);
+    }
+
+    //TODO criar método de update
+
+    //TODO exemplo com lista criada aqui na controller sem uso do repository
+
+//    @GetMapping
+//    public String listAccountsTest() {
+//        return "Hello World";
+//    }
+//
+//    @GetMapping("/{id}")
+//    public String getAccountByIdPathVariableTest(@PathVariable Integer id) {
+//        return "Hello World by id: " + id;
+//    }
+//
+//    @GetMapping("/filter")
+//    public String getAccountByIdPathParamTest(@RequestParam Integer id) {
+//        return "Hello World by id: " + id;
+//    }
+
+
 
 }
